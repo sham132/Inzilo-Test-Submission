94a381eb95cf5144d53a857802d510b3a498399e
sham132 <37959920+sham132@users.noreply.github.com>
2019-08-20 05:06:27 +0500

    Add files via upload

---
+++ hooks/hooks.js
@@ -0,0 +1,11 @@
+
+
+const  ValidateEmail= (mail)=> {
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+            return (true)
+        } else {
+            return (false)
+        }
+
+    }
+module.exports = ValidateEmail;
